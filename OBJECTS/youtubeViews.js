91a1f40069b3d1dfffac77d7e599c289c8b7a341
fhrizwi <faizulhaque2002@gmail.com>
2025-05-29 00:11:09 +0530

    updated

--- OBJECTS/youtubeViews.js
+++ OBJECTS/youtubeViews.js
@@ -1,14 +1,26 @@
 // 6.Create a function that takes an object YouTube video and returns true if the video has more than 1000 views.
 
-const youtubeVideos={
-    title:"js in one shot",
-    channel:"Alpha Intern",
-    views:1000
-}
-function getVideosViews(video){
-    // console.log(youtubeVideos.views)
-    return youtubeVideos.views
+// const youtubeVideos={
+//     title:"js in one shot",
+//     channel:"Alpha Intern",
+//     views:1000
+// }
+// function getVideosViews(video){
+//     // console.log(youtubeVideos.views)
+//     return youtubeVideos.views
+// }
+
+// // getVideosViews(youtubeVideos)
+// console.log(getVideosViews(youtubeVideos))
+
+const youtubeVideos = {
+    title: "js in one shot",
+    channel: "Alpha Intern",
+    views: 1000
+};
+
+function getVideosViews(video) {
+    return video.views > 1000;
 }
 
-// getVideosViews(youtubeVideos)
-console.log(getVideosViews(youtubeVideos))
+console.log(getVideosViews(youtubeVideos));  // false
